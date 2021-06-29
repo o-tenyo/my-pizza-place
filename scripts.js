8f0542b974d35021693d057cd6edd674d0481e06
o-tenyo <johnstone.omundi@student.moringaschool.comgit config --global user.email johnstone.omundi@student.moringaschool.com>
2021-06-29 22:54:48 +0300

    Created function to assign prices accodring to type oof crust selected

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
+//defining some variables
 let priceOfSize;
 let priceOfCrust;
 let priceOfTopping;
-
+let numberOfPizza;
+let priceOfPizza;
+let deliveryFee = 250;
+//this function returns the price of a specific pizza size
 function pricebySize(){
     if(document.getElementById('small').checked){   
         let size = document.getElementById('small');
@@ -24,55 +28,77 @@ function pricebySize(){
         return priceOfSize;
     }
 }
-
+//this function returns the price of a specific pizza crust
 function pricebyCrust(){
     if(document.getElementById('crispy').checked){   
-        let size = document.getElementById('crispy');
+        let crust = document.getElementById('crispy');
         priceOfCrust = 220;
         return priceOfCrust;
     }
     else if(document.getElementById('stuffed').checked){    
-        let size = document.getElementById('stuffed');
+        let crust = document.getElementById('stuffed');
         priceOfCrust = 300;
         return priceOfCrust;
     }
     else if(document.getElementById('sicillian').checked){
-        let size = document.getElementById('sicillian');
+        let crust = document.getElementById('sicillian');
         priceOfCrust = 190;
         return priceOfCrust;
     }
     else if(document.getElementById('gluten-free').checked){
-        let size = document.getElementById('gluten-free');
+        let crust = document.getElementById('gluten-free');
         priceOfCrust = 350;
         return priceOfCrust;
     }
     else if(document.getElementById('thick').checked){
-        let size = document.getElementById('thick');
+        let crust = document.getElementById('thick');
         priceOfCrust = 400;
         return priceOfCrust;
     }
 }
-
-function submissionClicked(){
-    if(document.getElementById('yes').checked){  
-        let delivery = document.getElementById('yes');
-        deliverFood();
+//this function returns the price of a specific pizza topping
+function pricebyToppings(){
+    if(document.getElementById('mushroom').checked){   
+        let toppings = document.getElementById('mushroom');
+        priceOfTopping = 110;
+        return priceOfTopping;
     }
-    if(document.getElementById('female').checked){    
-        let delivery = document.getElementById('no');
-        eatAtRestaurant();
+    else if(document.getElementById('pepperoni').checked){    
+        let toppings = document.getElementById('pepperoni');
+        priceOfTopping = 1050;
+        return priceOfTopping;
+    }
+    else if(document.getElementById('onion').checked){
+        let toppings = document.getElementById('onion');
+        priceOfTopping = 130;
+        return priceOfTopping;
+    }
+    else if(document.getElementById('sausage').checked){
+        let toppings = document.getElementById('sausage');
+        priceOfTopping = 120;
+        return priceOfTopping;
     }
 }
 
+function deliverFood(){
+    numberOfPizza = document.getElementById('num');
+    priceOfPizza = deliveryFee + (numberOfPizza * (pricebySize() + pricebyCrust() + pricebyToppings()));
+}
+function eatAtRestaurant(){
+    pnumberOfPizza = document.getElementById('num');
+    priceOfPizza = numberOfPizza * (pricebySize() + pricebyCrust() + pricebyToppings());
+}
 
-
-
-
-// function deliverFood(){
-    
-// }
-
-// function eatAtRestaurant(){
-
-// }
-
+function submissionClicked(){
+    // console.log('Works!!');
+    if(document.getElementById('yes').checked){  
+        let delivery = document.getElementById('yes');
+        // console.log('yes')
+        deliverFood();
+    }
+    else if(document.getElementById('no').checked){    
+        let delivery = document.getElementById('no');
+        // console.log('no')
+        eatAtRestaurant();
+    }
+}
